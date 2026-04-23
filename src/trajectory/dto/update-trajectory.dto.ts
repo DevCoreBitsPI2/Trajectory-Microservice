b522f9a0711ad07b8821b55aa3c1ad1d015308b4
Joseligos <joseligo14@gmail.com>
2026-04-22 19:39:17 -0500

    borrado carpeta innecesaria trajectory

--- src/trajectory/dto/update-trajectory.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTrajectoryDto } from './create-trajectory.dto';
-
-export class UpdateTrajectoryDto extends PartialType(CreateTrajectoryDto) {
-  id: number;
-}
